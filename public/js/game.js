5643007720bccf11f96a2f45af7e10a94f693b26
Harikrushna V Adiecha <adiechahari@gmail.com>
2020-04-19 00:30:32 +0530

    board change and few more changes

--- public/js/game.js
+++ public/js/game.js
@@ -3,17 +3,35 @@ const vueConf = {
 	data: {
 		greet: "Hey !",
 		board: [],
-		status: "waiting"
+		manLeft: 9,
+		color: "blue",
+		opColor: "green",
+		status: "waiting",
+		moves: []
 	},
 	mounted() {
 		let data = JSON.parse($(this.$refs.boardHolder).attr('data'));
 		this.board = data.hasOwnProperty('board') ? data.board: [];
+		this.color = data.hasOwnProperty('color') ? data.color: "blue";
+		this.opColor = data.hasOwnProperty('opColor') ? data.color: "green";
 	},
 	methods: {
 		markSpot(x, y) {
 			if(this.hasSpot(x, y)) {
-
+				/* Check if man left */
+				if(this.manLeft > 0) {
+					this.occupySpot(x, y);
+				}
+				console.log("Yes, it's spot")
+				return;
 			}
+			console.error("Oops it's not a spot !");
+		},
+		occupySpot(x, y) {
+			let cur = this.getBoardCell(x, y);
+			cur += " " + this.color;
+			console.log(cur);
+			this.setBoardCell(x, y, cur);
 		},
 		computePoints(x, y){
 			let lineCoordinates = {
@@ -38,11 +56,17 @@ const vueConf = {
 			return this.hasSpot(x, y)? 10: 0;
 		},
 		getSpotColor(x, y) {
+			let cell = this.getBoardCell(x, y);
+			let colors = [this.color, this.opColor].filter(c => cell.indexOf(c) != -1);
+			if (colors.length > 0) return colors[0];
 			return "white";
 		},
 		getBoardCell(x, y) {
 			let row = this.board.length > x ? this.board[x]: [];
 			return row.length > y ? row[y]: "";
+		},
+		setBoardCell(x, y, val) {
+			this.board[x][y] = val;
 		}
 	}
 };
--- statics/board.js
+++ statics/board.js
@@ -5,108 +5,70 @@ exports = module.exports = {
 			"right down spot", // Col #1 (R1) 
 			"left right", // Col #2 (R1) 
 			"left right", // Col #3 (R1) 
-			"left right", // Col #4 (R1) 
-			"left right spot down", // Col #5 (R1) 
+			"left right spot down", // Col #4 (R1) 
+			"left right", // Col #5 (R1) 
 			"left right", // Col #6 (R1) 
-			"left right", // Col #7 (R1) 
-			"left right", // Col #8 (R1) 
-			"left down spot", // Col #9 (R1) 
+			"left down spot", // Col #7 (R1) 
 		],
 		/* Row #2 */
 		[
 			"up down", // Col #1 (R2) 
 			"down right spot", // Col #2 (R2) 
 			"left right", // Col #3 (R2) 
-			"left right", // Col #4 (R2) 
-			"left right spot up down", // Col #5 (R2) 
-			"left right", // Col #6 (R2) 
-			"left right", // Col #7 (R2) 
-			"left down spot", // Col #8 (R2) 
-			"up down", // Col #9 (R2) 
+			"left right spot up down", // Col #4 (R2) 
+			"left right", // Col #5 (R2) 
+			"left down spot", // Col #6 (R2) 
+			"up down", // Col #7 (R2) 
 		],
 		/* Row #3 */
 		[
 			"up down", // Col #1 (R3) 
 			"up down", // Col #2 (R3) 
 			"down right spot", // Col #3 (R3) 
-			"left right", // Col #4 (R3) 
-			"left right spot up", // Col #5 (R3) 
-			"left right", // Col #6 (R3) 
-			"left down spot", // Col #7 (R3) 
-			"up down", // Col #8 (R3) 
-			"up down", // Col #9 (R3) 
+			"left right spot up", // Col #4 (R3) 
+			"left down spot", // Col #5 (R3) 
+			"up down", // Col #6 (R3) 
+			"up down", // Col #7 (R3) 
 		],
 		/* Row #4 */
 		[
-			"up down", // Col #1 (R4) 
-			"up down", // Col #2 (R4) 
-			"up down", // Col #3 (R4) 
+			"up down spot right", // Col #1 (R4) 
+			"up down spot right left", // Col #2 (R4) 
+			"up down spot left", // Col #3 (R4) 
 			"", // Col #4 (R4) 
-			"", // Col #5 (R4) 
-			"", // Col #6 (R4) 
-			"up down", // Col #7 (R4) 
-			"up down", // Col #8 (R4) 
-			"up down", // Col #9 (R4) 
+			"up down spot right", // Col #5 (R4) 
+			"up down spot left right", // Col #6 (R4) 
+			"up down spot left", // Col #7 (R4) 
 		],
 		/* Row #5 */
 		[
-			"up down spot right", // Col #1 (R5) 
-			"up down spot right left", // Col #2 (R5) 
-			"up down spot left", // Col #3 (R5) 
-			"", // Col #4 (R5) 
-			"", // Col #5 (R5) 
-			"", // Col #6 (R5) 
-			"up down spot right", // Col #7 (R5) 
-			"up down spot left right", // Col #8 (R5) 
-			"up down spot left", // Col #9 (R5) 
+			"up down", // Col #1 (R5) 
+			"up down", // Col #2 (R5) 
+			"up right spot", // Col #3 (R5) 
+			"left right spot down", // Col #4 (R5) 
+			"left up spot", // Col #5 (R5) 
+			"up down", // Col #6 (R5) 
+			"up down", // Col #7 (R5) 
 		],
 		/* Row #6 */
 		[
 			"up down", // Col #1 (R6) 
-			"up down", // Col #2 (R6) 
-			"up down", // Col #3 (R6) 
-			"", // Col #4 (R6) 
-			"", // Col #5 (R6) 
-			"", // Col #6 (R6) 
+			"up right spot", // Col #2 (R6) 
+			"left right", // Col #3 (R6) 
+			"left right spot up down", // Col #4 (R6) 
+			"left right", // Col #5 (R6) 
+			"left up spot", // Col #6 (R6) 
 			"up down", // Col #7 (R6) 
-			"up down", // Col #8 (R6) 
-			"up down", // Col #9 (R6) 
 		],
 		/* Row #7 */
 		[
-			"up down", // Col #1 (R7) 
-			"up down", // Col #2 (R7) 
-			"up right spot", // Col #3 (R7) 
-			"left right", // Col #4 (R7) 
-			"left right spot down", // Col #5 (R7) 
+			"right up spot", // Col #1 (R7) 
+			"left right", // Col #2 (R7) 
+			"left right", // Col #3 (R7) 
+			"left right spot up", // Col #4 (R7) 
+			"left right", // Col #5 (R7) 
 			"left right", // Col #6 (R7) 
 			"left up spot", // Col #7 (R7) 
-			"up down", // Col #8 (R7) 
-			"up down", // Col #9 (R7) 
-		],
-		/* Row #8 */
-		[
-			"up down", // Col #1 (R2) 
-			"up right spot", // Col #2 (R2) 
-			"left right", // Col #3 (R2) 
-			"left right", // Col #4 (R2) 
-			"left right spot up down", // Col #5 (R2) 
-			"left right", // Col #6 (R2) 
-			"left right", // Col #7 (R2) 
-			"left up spot", // Col #8 (R2) 
-			"up down", // Col #9 (R2) 
-		],
-		/* Row #9 */
-		[
-			"right up spot", // Col #1 (R1) 
-			"left right", // Col #2 (R1) 
-			"left right", // Col #3 (R1) 
-			"left right", // Col #4 (R1) 
-			"left right spot up", // Col #5 (R1) 
-			"left right", // Col #6 (R1) 
-			"left right", // Col #7 (R1) 
-			"left right", // Col #8 (R1) 
-			"left up spot", // Col #9 (R1) 
 		]
 	]
 }
